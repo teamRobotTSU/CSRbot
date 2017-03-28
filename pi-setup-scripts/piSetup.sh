2f995297746ce36f52b99cc5e3b2ebde113f4ca7
teamRobotTSU <tdarrah@my.tnstate.edu>
2017-03-28 15:59:44 -0500

    Update piSetup.sh

--- pi-setup-scripts/piSetup.sh
+++ pi-setup-scripts/piSetup.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
 
 cd ~
+echo "**************************************************"
 echo "****************   UPDATING   ********************"
+echo "**************************************************"
 sudo apt-get update -y
 
+echo "********************************************************"
 echo "****************   INSTALLING PIP   ********************"
+echo "********************************************************"
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 sudo python3 get-pip.py
 
-echo "****************   INSTALING PYTHON PACKAGES   ********************"
+echo "********************************************************************"
+echo "****************   INSTALING PYTHON3 PACKAGES   ********************"
+echo "********************************************************************"
 sudo pip install numpy
 sudo pip3 install numpy
+sudo pip3 install imutils
+sudo pip3 install scipy
 
+echo "********************************************************************"
+echo "****************   INSTALLING PYTHON2 PACKAGES  ********************"
+echo "********************************************************************"
 sudo apt-get install i2c-tools python-smbus python-opengl python-pygame python-webpy -y
 
+echo "*************************************************************"
 echo "****************   INSTALLING VNC SERVER ********************"
-sudo apt-get install tightvncserver
-cd /home/$USER/.config
-mkdir autostart
-cd autostart
-echo "[Desktop Entry] Type=Application Name=TightVNC Exec=vncserver :1 StartupNotify=false" > tightvnc.desktop
+echo "*************************************************************"
+sudo apt-get install tightvncserver -y
+printf "vncserver :1" >> .bashrc
 
-
+echo "********************************************************************"
 echo "****************   CHANGING DESKTOP BACKGROUND  ********************"
+echo "********************************************************************"
 pcmanfm --set-wallpaper ~/csrbot/pi-setup-scripts/robotBackground.png
 
 echo "the installer script has completed."
