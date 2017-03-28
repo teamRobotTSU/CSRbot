334ceec578f87ffb920cb8cce48a37528eff6a33
teamRobotTSU <tdarrah@my.tnstate.edu>
2017-03-28 11:52:36 -0500

    reorganized the files

---
+++ robot-api/Hardware.py
@@ -0,0 +1,124 @@
+import RPi.GPIO as GPIO
+import smbus
+import os
+import sys
+import termios
+import tty
+
+#bus = smbus.SMBus(1)
+#gyroAddress = 0x68
+
+Motor1E = 33
+Motor1A = 35
+Motor1B = 37
+
+Motor2E = 18
+Motor2A = 16
+Motor2B = 12
+
+buzzer = 29
+greenLED = 36
+green1LED = 32
+yellowLED = 38
+redLED = 40
+panServo = 7
+tiltServo = 11
+
+ECHO = 13
+TRIG = 22
+
+pir = 15
+
+PhotoPin = 24
+PhotoPin2 = 10
+
+RIRLED = 19
+LIRLED =31
+LIREC = 8
+RIREC = 26
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(Motor2E, GPIO.OUT)
+GPIO.setup(Motor2A, GPIO.OUT)
+GPIO.setup(Motor2B, GPIO.OUT)
+GPIO.setup(Motor1E, GPIO.OUT)
+GPIO.setup(Motor1A, GPIO.OUT)
+GPIO.setup(Motor1B, GPIO.OUT)
+pwm1 = GPIO.PWM(Motor1E, 100)
+pwm2 = GPIO.PWM(Motor2E, 100)
+
+GPIO.setup(buzzer, GPIO.OUT)
+GPIO.setup(greenLED, GPIO.OUT)
+GPIO.setup(green1LED, GPIO.OUT)
+GPIO.setup(yellowLED, GPIO.OUT)
+GPIO.setup(redLED, GPIO.OUT)
+GPIO.setup(panServo, GPIO.OUT)
+GPIO.setup(tiltServo, GPIO.OUT)
+
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+GPIO.output(TRIG, False)
+
+GPIO.setup(pir, GPIO.IN)
+
+GPIO.setup(PhotoPin, GPIO.OUT)
+GPIO.setup(PhotoPin2, GPIO.OUT)
+
+GPIO.setup(RIRLED,GPIO.OUT)
+GPIO.setup(LIRLED,GPIO.OUT)
+GPIO.setup(LIREC, GPIO.IN)
+GPIO.setup(RIREC, GPIO.IN)
+
+
+def getKey():
+    fd = sys.stdin.fileno()
+    old = termios.tcgetattr(fd)
+    new = termios.tcgetattr(fd)
+    new[3] = new[3] & ~termios.ICANON & ~termios.ECHO
+    new[6][termios.VMIN] = 1
+    new[6][termios.VTIME] = 0
+    termios.tcsetattr(fd, termios.TCSANOW, new)
+    k = None
+    try:
+        k = os.read(fd, 3)
+    finally:
+        termios.tcsetattr(fd, termios.TCSAFLUSH, old)
+    key = str(k)
+    key = key.replace("b", "")
+    key = key.replace("'", "")
+    return key
+
+def hardwareCleanup():
+    GPIO.cleanup()
+    return
+
+def tilt(direction):
+    if direction == "u":
+        os.system("echo 1=+1 > /dev/servoblaster")
+    elif direction == "c":
+        os.system("echo 1=150 > /dev/servoblaster")
+    elif direction == "d":
+        os.system("echo 1=-1 > /dev/servoblaster")
+    return
+
+def pan(direction):
+    if direction == "l":
+        os.system("echo 2=+1 > /dev/servoblaster")
+    elif direction == "c":
+        os.system("echo 2=150 > /dev/servoblaster")
+    elif direction == "r":
+        os.system("echo 2=-1 > /dev/servoblaster")
+    return
+
+#os.system("sudo /home/pi/csrbot1/PiBits/ServoBlaster/user/servod")
+#os.system("echo 1=150 > /dev/servoblaster")
+#os.system("echo 2=150 > /dev/servoblaster")
+
+
+
+
+    
+
+
+
+
---
+++ robot-api/Sense.py
@@ -0,0 +1,126 @@
+import RPi.GPIO as GPIO
+import time
+from Hardware import *
+
+
+
+class Sense:
+  def __init__(self):
+    print("Object Created")
+    self.ECHO = ECHO
+    self.TRIG = TRIG
+    
+    self.ledA = redLED
+    self.ledB = yellowLED
+
+    self.pir = pir
+
+    self.PhotoPin = PhotoPin
+
+    self.PhotoPin2 = PhotoPin2
+
+    self.RIRLED = RIRLED
+    self.LIRLED = LIRLED
+
+
+
+    self.LIREC = LIREC
+    self.RIREC = RIREC
+    
+  def SonicRange(self):
+    print ("Distance Measurement In Progress")
+    print ("Allow Module to Settle")
+    time.sleep(0.5)
+    print ("Settled")
+    GPIO.output(self.TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(self.TRIG,False)
+    pulse_start = time.time()
+    while GPIO.input(self.ECHO)==0:
+      pulse_start = time.time()
+    while GPIO.input(self.ECHO)==1:
+      pulse_end = time.time()
+    pulse_duration = pulse_end-pulse_start
+    distance = pulse_duration*17150
+    return distance
+  
+  def LED(self):
+    i = 0
+    GPIO.output(self.ledA, 1)
+    GPIO.output(self.ledB,1)
+    time.sleep(1)
+    GPIO.output(self.ledA,0)
+    GPIO.output(self.ledB,0)
+    
+  def PIR(self):
+    status = True
+    while status:
+      i = GPIO.input(self.pir)
+      if i == 0:
+        print (" No intruders")
+      elif i == 1:
+        print ("Intruders")
+        self.LED()
+        status = False
+        
+  def PhotoRes(self):
+    count = 0
+    GPIO.output(self.PhotoPin,GPIO.LOW)
+    time.sleep(0.1)
+    GPIO.setup(self.PhotoPin,GPIO.IN)
+    while (GPIO.input(self.PhotoPin) == GPIO.LOW):
+      count+=1
+    return count
+
+  def PhotoRes2(self):
+    count = 0
+    GPIO.output(self.PhotoPin2,GPIO.LOW)
+    time.sleep(0.1)
+    GPIO.setup(self.PhotoPin2,GPIO.IN)
+    while (GPIO.input(self.PhotoPin2) == GPIO.LOW):
+      ##print ("Enter Loop")
+      count+=1
+    return count
+
+  def IRTrans(self):
+    GPIO.output(self.LIRLED, 1)
+    GPIO.output(self.RIRLED,1)
+    self.IRRec()
+    GPIO.output(self.LIRLED,0)
+    GPIO.output(self.RIRLED,0)
+    
+  def IRRec(self):
+    stat = True
+    newcount = 0
+    while stat:
+
+      newcount+=1
+      LeftInput = GPIO.input(self.LIREC)
+      RightInput = GPIO.input(self.RIREC)
+      if LeftInput == 1 and RightInput == 1:
+        print ("Surface there")
+      elif LeftInput == 0 or RightInput == 0:
+        print ("No Surface | RightInput =", RightInput, "Left Input =", LeftInput, "count", newcount)
+        ##self.LED()
+        stat = False
+
+def destroy():
+  GPIO.cleanup()
+    
+def main():
+  a = Sensors()
+  try:
+    print("Distance from object", a.SonicRange(), "cm")
+    a.LED()
+    ##a.PIR()
+    print("Time to charge short Capacitor", a.PhotoRes(), "loops")
+    print("Time to charge tall Capacitor", a.PhotoRes2(), "loops")
+    ##a.IRTrans()
+    ##a.IRRec()
+  except KeyboardInterrupt:
+    destroy()
+  finally:
+    destroy()
+  
+if __name__ == '__main__':
+  main()
