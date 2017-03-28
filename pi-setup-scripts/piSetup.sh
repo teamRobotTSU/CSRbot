09e0a4688b0620c8318cf6c1f1d06705694b7489
teamRobotTSU <tdarrah@my.tnstate.edu>
2017-03-28 14:59:46 -0500

    Update piSetup.sh

--- pi-setup-scripts/piSetup.sh
+++ pi-setup-scripts/piSetup.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 cd ~
+echo "****************   UPDATING   ********************"
 sudo apt-get update -y
+
+echo "****************   INSTALLING PIP   ********************"
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 sudo python3 get-pip.py
+
+echo "****************   INSTALING NUMPY   ********************"
 sudo pip install numpy
 sudo pip3 install numpy
+
+echo "****************   INSTALLING I2C-TOOLS   ********************"
 sudo apt-get install i2c-tools -y
+
+echo "****************   CHANGING DESKTOP BACKGROUND  ********************"
+pcmanfm --set-wallpaper ~/csrbot/pi-setup-scripts/robotBackground.png
 
 echo "the installer script has completed."
