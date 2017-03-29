315bd8e079a3ba061ead6aa79a10708b605312ec
teamRobotTSU <tdarrah@my.tnstate.edu>
2017-03-29 15:40:12 -0500

    Update buildOpenCV.sh

--- pi-setup-scripts/buildOpenCV.sh
+++ pi-setup-scripts/buildOpenCV.sh
@@ -3,19 +3,7 @@
 cd ~
 sudo apt-get update -y
 sudo apt-get install build-essential cmake pkg-config -y
-sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev -y
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
-sudo apt-get install libxvidcore-dev libx264-dev -y
-sudo apt-get install libgtk2.0-dev -y
-sudo apt-get install libatlas-base-dev gfortran -y
-sudo apt-get install python2.7-dev python 3-dev python3.4-dev -y
-wget https://bootstrap.pypa.io/get-pip.py
 
-#it is assumed you have these already after setting up the pi
-#sudo python get-pip.py
-#sudo python3 get-pip.py
-#sudo pip install numpy
-#sudo pip3 install numpy
 wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.1.0.zip
 wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.1.0.zip
 unzip opencv.zip
@@ -31,5 +19,6 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
 make
 sudo make install
 sudo ldconfig
+echo "your cv version is (should be 3.1.0):"
 python3 /home/$USER/csrbot/robot-scripts/cvVersion.py
 echo "the installer script has completed."
